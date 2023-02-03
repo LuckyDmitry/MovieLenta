35707e026c9e2f7dd159b377f96df59dca863f9a
Dmitry Trifonov <dim.trif1234@gmail.com>
2023-02-03 11:38:51 +0300

    Initial Commit

---
+++ MovieLenta/MovieLentaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieLentaApp.swift
+//  MovieLenta
+//
+//  Created by Dmitry Trifonov on 03.02.23.
+//
+
+import SwiftUI
+
+@main
+struct MovieLentaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
